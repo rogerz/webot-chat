903cf2e9e6f8b07ee59699ed625abdcbf96b6baa
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-01-06 21:43:43 +0800

    Initial test case

--- test/webot-chat_test.js
+++ test/webot-chat_test.js
@@ -1,36 +1,10 @@
 'use strict';
 
-var webot-chat = require('../lib/webot-chat.js');
+var webotChat = require('../lib/webot-chat.js');
+require('should');
 
-/*
-  ======== A Handy Little Mocha Reference ========
-  https://github.com/visionmedia/mocha/
-
-  Test assertions:
-    assert.fail(actual, expected, message, operator)
-    assert(value, message), assert.ok(value, [message])
-    assert.equal(actual, expected, [message])
-    assert.notEqual(actual, expected, [message])
-    assert.deepEqual(actual, expected, [message])
-    assert.notDeepEqual(actual, expected, [message])
-    assert.strictEqual(actual, expected, [message])
-    assert.notStrictEqual(actual, expected, [message])
-    assert.throws(block, [error], [message])
-    assert.doesNotThrow(block, [message])
-    assert.ifError(value)
-
-    Apart from assert, Mocha allows you to use any of the following assertion libraries:
-    - should.js
-    - chai
-    - expect.js
-    - better-assert
-*/
-
-var assert = require('assert');
-
-suite('AwesomenessTest', function(){
-  test('#awesome()', function(done){
-    assert.equal(webot-chat.awesome(), 'awesome');
-    done();
+describe('webot-chat', function () {
+  it('should export object', function () {
+    webotChat.should.be.type('object');
   });
 });
